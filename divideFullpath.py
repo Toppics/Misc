721a7e6208bbfb8990b2d936de0440997ec8a10c
yan-s <110732997+Toppics@users.noreply.github.com>
2023-02-21 16:20:07 +0100

    Update divideFullpath.py

--- divideFullpath.py
+++ divideFullpath.py
@@ -7,7 +7,7 @@ Created on Tue Aug  9 00:35:02 2022
 
 import os
 
-fullpath = r'C:/Users/yan-s/Documents/pypy/Location Geography & Co/Sine Saloum/repo/Sine Saloum 2.0.kml'
+fullpath = r'C:/Users/yan-s/Sine Saloum/repo/Sine Saloum 2.0.kml'
 
 dirname, fname = os.path.split(fullpath)
 
